b56efd5b9f2e1b0b077166f78be3414bcb4f264a
Anh Do <doducanh2710@gmail.com>
2018-06-11 15:04:14 +0700

    Communicate frontend and backend
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

--- backend/gameapi/application.py
+++ backend/gameapi/application.py
@@ -4,10 +4,15 @@ application.py
 """
 
 from flask import Flask
+from flask_cors import CORS
 
 def create_app(app_name='GAME_API'):
     app = Flask(app_name)
     app.config.from_object('gameapi.config.BaseConfig')
+
+    cors = CORS(app, resource={r"/api/*": {"origin": "*"}})
+
     from gameapi.api import api
     app.register_blueprint(api, url_prefix='/api')
+
     return app
---
+++ frontend/worldcup_prediction_game_spa/src/api/index.js
@@ -0,0 +1,189 @@
+const matches = [{
+    num: 2,
+    date: "2014-06-13",
+    time: "13:00",
+    team1: {
+      name: "Mexico",
+      code: "MEX"
+    },
+    team2: {
+      name: "Cameroon",
+      code: "CMR"
+    },
+    score1: 1,
+    score2: 0,
+    score1i: null,
+    score2i: null,
+    goals: [{
+      name: "Oribe Peralta",
+      team: {
+        name: "Mexico",
+        code: "MEX"
+      },
+      minute: 61,
+      score1: 1,
+      score2: 0
+    }],
+    group: "Group A"
+  },
+  {
+    num: 3,
+    date: "2014-06-13",
+    time: "16:00",
+    team1: {
+      name: "Spain",
+      code: "ESP"
+    },
+    team2: {
+      name: "Netherlands",
+      code: "NED"
+    },
+    score1: 1,
+    score2: 5,
+    score1i: null,
+    score2i: null,
+    goals: [{
+        name: "Xabi Alonso",
+        team: {
+          name: "Spain",
+          code: "ESP"
+        },
+        minute: 27,
+        score1: 1,
+        score2: 0,
+        penalty: true
+      },
+      {
+        name: "Robin Van Persie",
+        team: {
+          name: "Netherlands",
+          code: "NED"
+        },
+        minute: 44,
+        score1: 1,
+        score2: 1
+      },
+      {
+        name: "Arjen Robben",
+        team: {
+          name: "Netherlands",
+          code: "NED"
+        },
+        minute: 53,
+        score1: 1,
+        score2: 2
+      },
+      {
+        name: "Stefan De Vrij",
+        team: {
+          name: "Netherlands",
+          code: "NED"
+        },
+        minute: 65,
+        score1: 1,
+        score2: 3
+      },
+      {
+        name: "Robin Van Persie",
+        team: {
+          name: "Netherlands",
+          code: "NED"
+        },
+        minute: 72,
+        score1: 1,
+        score2: 4
+      },
+      {
+        name: "Arjen Robben",
+        team: {
+          name: "Netherlands",
+          code: "NED"
+        },
+        minute: 80,
+        score1: 1,
+        score2: 5
+      }
+    ],
+    group: "Group B",
+    stadium: {
+      key: "fontenova",
+      name: "Arena Fonte Nova"
+    },
+    city: "Salvador",
+    timezone: "UTC-3"
+  },
+  {
+    num: 4,
+    date: "2014-06-13",
+    time: "18:00",
+    team1: {
+      name: "Chile",
+      code: "CHI"
+    },
+    team2: {
+      name: "Australia",
+      code: "AUS"
+    },
+    score1: 3,
+    score2: 1,
+    score1i: null,
+    score2i: null,
+    goals: [{
+        name: "Alexis Sánchez",
+        team: {
+          name: "Chile",
+          code: "CHI"
+        },
+        minute: 12,
+        score1: 1,
+        score2: 0
+      },
+      {
+        name: "Jorge Valdívia",
+        team: {
+          name: "Chile",
+          code: "CHI"
+        },
+        minute: 14,
+        score1: 2,
+        score2: 0
+      },
+      {
+        name: "Cahill",
+        team: {
+          name: "Australia",
+          code: "AUS"
+        },
+        minute: 35,
+        score1: 2,
+        score2: 1
+      },
+      {
+        name: "Jean Beausejour",
+        team: {
+          name: "Chile",
+          code: "CHI"
+        },
+        minute: 90,
+        offset: 2,
+        score1: 3,
+        score2: 1
+      }
+    ],
+    group: "Group B",
+    stadium: {
+      key: "pantanal",
+      name: "Arena Pantanal"
+    },
+    city: "Cuiabá",
+    timezone: "UTC-4"
+  }
+]
+
+import axios from 'axios'
+
+const API_URL = 'http://127.0.0.1:5000/api'
+
+export function fetchMatches() {
+    return axios.get(`${API_URL}/get/matches`)
+}
