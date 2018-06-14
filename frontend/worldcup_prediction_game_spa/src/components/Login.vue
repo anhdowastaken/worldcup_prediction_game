1aa465e4f450ccc1bbb168561a2101810bdf5ffb
Anh Do <doducanh2710@gmail.com>
2018-06-14 08:47:36 +0700

    Update style for Login component
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

--- frontend/worldcup_prediction_game_spa/src/components/Login.vue
+++ frontend/worldcup_prediction_game_spa/src/components/Login.vue
@@ -1,9 +1,15 @@
 <template>
-    <div id="form-login">
-        <h1>Login</h1>
-        <input type="text" name="username" v-model="username" placeholder="Username" />
-        <input type="password" name="password" v-model="password" placeholder="Password" />
-        <button type="button" v-on:click="login()">Login</button>
+    <div class="container">
+
+      <form class="form-signin">
+        <h2 class="form-signin-heading"></h2>
+        <label for="inputUsername" class="sr-only">Username</label>
+        <input type="username" id="inputUsername" class="form-control" placeholder="Username" required autofocus v-model="username">
+        <label for="inputPassword" class="sr-only">Password</label>
+        <input type="password" id="inputPassword" class="form-control" placeholder="Password" required v-model="password">
+        <button class="btn btn-lg btn-primary btn-block" type="submit" v-on:click="login()" v-on:submit="login()">Login</button>
+      </form>
+
     </div>
 </template>
 
@@ -26,4 +32,38 @@ export default {
 </script>
 
 <style scoped>
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+  margin: 0 auto;
+}
+.form-signin .form-signin-heading,
+.form-signin .checkbox {
+  margin-bottom: 10px;
+}
+.form-signin .checkbox {
+  font-weight: normal;
+}
+.form-signin .form-control {
+  position: relative;
+  height: auto;
+  -webkit-box-sizing: border-box;
+     -moz-box-sizing: border-box;
+          box-sizing: border-box;
+  padding: 10px;
+  font-size: 16px;
+}
+.form-signin .form-control:focus {
+  z-index: 2;
+}
+.form-signin input[type="username"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
 </style>
