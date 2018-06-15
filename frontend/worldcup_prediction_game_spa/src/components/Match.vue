c357c4f2ec9badc78f4ce5e054d21b080cdc242c
Anh Do <doducanh2710@gmail.com>
2018-06-15 15:05:44 +0700

    Update UI for Match component
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

--- frontend/worldcup_prediction_game_spa/src/components/Match.vue
+++ frontend/worldcup_prediction_game_spa/src/components/Match.vue
@@ -1,41 +1,52 @@
 <template>
     <div class="col-lg-6 match">
-        <span v-if="didMatchEnd && isPredictionCorrect"
-              class="result-icon fa fa-thumbs-o-up" style="color:blue"></span>
-        <span v-if="didMatchEnd && !isPredictionCorrect"
-              class="result-icon fa fa-thumbs-o-down" style="color:red"></span>
-        <div class="match-detail">
-            <div class="group">
-                <small>[{{ this.match.group }}]</small>
-            </div>
-            <div class="match-title">
-                <h4 v-if="!didMatchEnd">{{ this.match.team1.name }} vs {{ this.match.team2.name }}</h4>
-                <h4 v-if="didMatchEnd">{{ this.match.team1.name }} {{ this.match.score1 }} - {{ this.match.score2 }} {{ this.match.team2.name }}</h4>
-            </div>
-            <div class="match-information">
-                <p>{{ this.match.local_match_time }}</p>
-                <p v-if="enoughTimeToPredict">{{ timeToPredict }} to predict</p>
-            </div>
-            <div class="match-result">
-            </div>
-        </div>
+        <div class="col-lg-12 match-container">
 
-        <div class="match-prediction">
-            <button type="button"
-                    class="btn btn-default"
-                    v-bind:class="{ 'btn-success': isTeam1Chosen, 'disabled': !enoughTimeToPredict }"
-                    v-bind:disabled="!enoughTimeToPredict"
-                    v-on:click.stop.prevent="submit(1)">{{ this.match.team1.code }}</button>
-            <button type="button"
-                    class="btn btn-default"
-                    v-bind:class="{ 'btn-success': isDrawChosen, 'disabled': !enoughTimeToPredict }"
-                    v-bind:disabled="!enoughTimeToPredict"
-                    v-on:click.stop.prevent="submit(0)">DRAW</button>
-            <button type="button"
-                    class="btn btn-default"
-                    v-bind:class="{ 'btn-success': isTeam2Chosen, 'disabled': !enoughTimeToPredict }"
-                    v-bind:disabled="!enoughTimeToPredict"
-                    v-on:click.stop.prevent="submit(2)">{{ this.match.team2.code }}</button>
+            <div class="background">
+                <p class="bg-text">{{ this.match.group }}</p>
+            </div>
+
+            <div class="content">
+                <span v-if="didMatchEnd && isPredictionCorrect"
+                      class="result-icon fa fa-thumbs-o-up" style="color:white"></span>
+                <span v-if="didMatchEnd && !isPredictionCorrect"
+                      class="result-icon fa fa-thumbs-o-down" style="color:red"></span>
+
+                <div class="match-detail">
+                    <div class="match-title">
+                        <p v-if="!didMatchEnd">{{ this.match.team1.name }} vs {{ this.match.team2.name }}</p>
+                        <p v-if="didMatchEnd">{{ this.match.team1.name }} {{ this.match.score1 }} - {{ this.match.score2 }} {{ this.match.team2.name }}</p>
+                    </div>
+                    <div class="match-result">
+                    </div>
+                    <div class="match-information">
+                        <p>{{ this.match.local_match_time }}</p>
+                    </div>
+                    <div class="match-predict-information">
+                        <p v-if="enoughTimeToPredict">{{ timeToPredict }} to predict!</p>
+                        <p v-else>This match can't be predicted</p>
+                    </div>
+                </div>
+
+                <div class="match-prediction">
+                    <button type="button"
+                            class="btn btn-default"
+                            v-bind:class="{ 'btn-success': isTeam1Chosen, 'disabled': !enoughTimeToPredict }"
+                            v-bind:disabled="!enoughTimeToPredict"
+                            v-on:click.stop.prevent="submit(1)">{{ this.match.team1.code }}</button>
+                    <button type="button"
+                            class="btn btn-default"
+                            v-bind:class="{ 'btn-success': isDrawChosen, 'disabled': !enoughTimeToPredict }"
+                            v-bind:disabled="!enoughTimeToPredict"
+                            v-on:click.stop.prevent="submit(0)">DRAW</button>
+                    <button type="button"
+                            class="btn btn-default"
+                            v-bind:class="{ 'btn-success': isTeam2Chosen, 'disabled': !enoughTimeToPredict }"
+                            v-bind:disabled="!enoughTimeToPredict"
+                            v-on:click.stop.prevent="submit(2)">{{ this.match.team2.code }}</button>
+                </div>
+            </div>
+
         </div>
     </div>
 </template>
@@ -163,7 +174,66 @@ export default {
 
 <style scoped>
 .match {
-  margin-top: 20px;
+    margin-bottom: 20px;
+}
+
+.match .match-container {
+    height: 210px;
+    min-height: 210px;
+}
+
+.match .background {
+    position: absolute;
+    z-index: -999999;
+    display: block;
+    min-height: 100%; 
+    min-width: 100%;
+    background: linear-gradient(to right, #B923A3, #863CBA, #024EA1, #004EA1);
+    padding-top: 10px;
+    padding-bottom: 10px;
+    border-style: solid;
+    border-width: 1px;
+    border-color: #707070;
+    border-radius: 15px;
+    /* opacity: 0.9; */
+    left: 0;
+}
+
+.match .background .bg-text {
+    color: lightgray;
+    font-size: 34px;
+    opacity: 0.3;
+    text-transform: uppercase;
+    position: absolute;
+    bottom: 0;
+    right: 0;
+    margin-right: 7px;
+    margin-bottom: 7px;
+    line-height: 80%;
+    font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+}
+
+.match .content {
+    color: #FFFFFF;
+    text-align: center;
+    padding: 10px;
+}
+
+.match .content .match-title {
+    margin-top: 30px;
+    font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+    font-size: 18px;
+    text-transform: uppercase;
+}
+.match .content .match-information {
+    font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+    font-size: 12px;
+    text-transform: uppercase;
+}
+
+.match .content .match-predict-information {
+    font-family: "Century Gothic", CenturyGothic, AppleGothic, sans-serif;
+    font-size: 10px;
 }
 .result-icon {
     float: right;
