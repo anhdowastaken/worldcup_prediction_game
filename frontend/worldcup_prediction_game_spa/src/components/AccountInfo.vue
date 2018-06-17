fe2703ed493b91525df5076cfb434039485f5dad
Anh Do <doducanh2710@gmail.com>
2018-06-17 23:00:59 +0700

    Update logic to calculate point
    - If an account was created after a match, that match will be ignored
    
    Signed-off-by: Anh Do <doducanh2710@gmail.com>

--- frontend/worldcup_prediction_game_spa/src/components/AccountInfo.vue
+++ frontend/worldcup_prediction_game_spa/src/components/AccountInfo.vue
@@ -43,6 +43,16 @@ export default {
 
             for (let i = 0; i < this.matches.length; i++) {
                 let match = this.matches[i]
+
+                // If this account is created after match started, don't count this match
+                let user_created_at = this.userData['created_at']
+                // Replace "-" by "/" to avoid issue on Safari
+                let match_time_str = match.date.replace(/-/g, "/") + ' ' + match.time + ' ' + (match.timezone ? match.timezone : '')
+                let match_time = new Date(match_time_str)
+                if (user_created_at * 1000 > match_time.getTime()) {
+                    continue
+                }
+
                 let prediction = match['prediction']
                 let score1 = (match['score1'] ? match['score1'] : 0)
                 score1 = score1 + (match['score1i'] ? match['score1i'] : 0)
@@ -53,7 +63,6 @@ export default {
                 score2 = score2 + (match['score2et'] ? match['score2et'] : 0)
                 score2 = score2 + (match['score2p'] ? match['score2p'] : 0)
 
-                // console.log(i + '-' + score1 + '-' + score2 + '-' + prediction)
                 if (match['score1'] == null || match['score1'] == undefined || match['score2'] == null || match['score2'] == undefined) {
                     continue
                 } else if (score1 == null || score1 == undefined || score2 == null || score2 == undefined) {
--- frontend/worldcup_prediction_game_spa/src/store/index.js
+++ frontend/worldcup_prediction_game_spa/src/store/index.js
@@ -223,7 +223,8 @@ const mutations = {
             // Backend returns timestamp in second (UTC)
             let d = new Date()
             // FIXME: Should force backend return timestamp of UTC+0?
-            d = new Date(payload.userData['last_login_at'] * 1000 - d.getTimezoneOffset() * 60 * 1000)
+            // d = new Date(payload.userData['last_login_at'] * 1000 - d.getTimezoneOffset() * 60 * 1000)
+            d = new Date(payload.userData['last_login_at'] * 1000)
             payload.userData['last_login_at'] = d.toLocaleString()
         }
         sessionStorage.setItem(key_user_data, JSON.stringify(payload.userData))
